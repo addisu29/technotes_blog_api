eb23bd8e05f5dd8c2ce5d56917be085b60031735
Addisu Merga <addisum2129@gmail.com>
2026-02-19 01:00:22 -0800

    Part 4: Migrated database, created superuser, and implemented Serializers and Admin views

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Tag, Post
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'is_published', 'created_at')
+    list_filter = ('is_published', 'category', 'author')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)} 
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Post, Category, Tag
+from .serializers import PostSerializer, CategorySerializer, TagSerializer
 
-# Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
