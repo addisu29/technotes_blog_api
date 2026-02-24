5d67cd6493ac0a18616da3afc8b4411347fcbdc7
Addisu Merga <addisum2129@gmail.com>
2026-02-24 05:53:38 -0800

    Added unit tests for Blog API

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Category
 
-# Create your tests here.
+class BlogAPITests(APITestCase):
+    
+    def setUp(self):
+        # Create a category 
+        self.category = Category.objects.create(name="Coding")
+
+    def test_view_posts(self):
+        """Test that we can access the posts endpoint"""
+        url = reverse('post-list') # This matches the router name
+        response = self.client.get(url)
+        
+        # Check if the status code is 200 (Success)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_category_exists(self):
+        """Test if the category was created correctly"""
+        cat = Category.objects.get(name="Coding")
+        self.assertEqual(cat.name, "Coding")
